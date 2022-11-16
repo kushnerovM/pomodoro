f9e8a74559649c3b8f614fb580c9daad78f8a328
kushnerovM <Rojers1941@yandex.by>
2022-11-16 10:23:00 +0300

    added basic functionality

---
+++ app.js
@@ -0,0 +1,11 @@
+//variables
+
+//selectors
+const timeLine = document.querySelector("#time-line");
+const pauseButton = document.querySelector("#pause");
+const stopButton = document.querySelector("#stop");
+//event listners
+
+//functions
+function tick(elem){
+}
