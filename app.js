61e0b62d5fa89b5522f2beac43fb3920b00204f2
kushnerovM <Rojers1941@yandex.by>
2022-11-16 11:12:27 +0300

    countdown added

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 //variables
-
+const second = 1000;
+const minute = second*60;
+//time-interval
+let interval = 45*minute;
+let gap = interval;
 //selectors
 const timeLine = document.querySelector("#time-line");
 const pauseButton = document.querySelector("#pause");
@@ -7,5 +11,11 @@ const stopButton = document.querySelector("#stop");
 //event listners
 
 //functions
-function tick(elem){
+const updateTime= ()=>timeLine.innerHTML=String(Math.trunc(gap/minute)).padStart(2,"0")+":"+String(gap%minute/second).padStart(2,"0");
+updateTime();
+function tick(){
+    gap-=1000;
+    console.log(String(gap%minute));
+    updateTime();
 }
+setInterval(tick,1000);
