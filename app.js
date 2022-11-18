6cdb5869ddf11a5f5e0c5ecdb12911ed93e69925
kushnerovM <Rojers1941@yandex.by>
2022-11-18 16:37:07 +0300

    pause fixed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const second = 1000;
 const minute = second*60;
 let isPaused = false;
+let timeInterval;
 //time-interval
 let interval = 45*minute;
 let gap = interval;
@@ -10,7 +11,17 @@ const timeLine = document.querySelector("#time-line");
 const pauseButton = document.querySelector("#pause");
 const stopButton = document.querySelector("#stop");
 //event listners
-pauseButton.addEventListener("click",()=>{isPaused=isPaused?false:true;})
+pauseButton.addEventListener("click",()=>{
+    if(isPaused){
+        pauseButton.textContent="pause";
+        timeInterval = setInterval(tick,1000);
+        isPaused= false;
+    } else {
+        pauseButton.textContent="resume";
+        clearInterval(timeInterval);
+        isPaused = true;
+    }
+})
 //functions
 const updateTime= ()=>timeLine.innerHTML=String(Math.trunc(gap/minute)).padStart(2,"0")+":"+String(gap%minute/second).padStart(2,"0");
 updateTime();
@@ -23,9 +34,10 @@ function tick(){
 //    setInterval(tick,1000);
 //}
 
-setTimeout(function run(){
+/*setTimeout(function run(){
     if(!isPaused){
         tick();
         setTimeout(run,1000)
     }
-},1000)
+},1000)*/
+timeInterval = setInterval(tick,1000);
