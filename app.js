23feb6747f098d548e44e877ea86d433271eb8ca
kushnerovM <Rojers1941@yandex.by>
2022-11-22 12:29:45 +0300

    full start/pause/stop interaction configured

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //variables
+
 const second = 1000;
 const minute = second*60;
 let isPaused = false;
@@ -6,11 +7,17 @@ let timeInterval;
 //time-interval
 let interval = 45*minute;
 let gap = interval;
+
 //selectors
+
 const timeLine = document.querySelector("#time-line");
 const pauseButton = document.querySelector("#pause");
 const stopButton = document.querySelector("#stop");
+const startButton = document.querySelector("#start");
+
 //event listners
+
+//pause/resume
 pauseButton.addEventListener("click",()=>{
     if(isPaused){
         pauseButton.textContent="pause";
@@ -23,28 +30,39 @@ pauseButton.addEventListener("click",()=>{
     }
 });
 
+//stop
 stopButton.addEventListener("click",()=>{
     clearInterval(timeInterval);
+    // set cirrent time-line time to default value
     updateTime(interval);
     gap = interval;
+    stopButton.style.display="none";
+    startButton.style.display="block";
+    pauseButton.style.display="none";
+    //pause reset if it was set
+    if(isPaused){
+        pauseButton.textContent="pause";
+        isPaused = false;
+    }
+});
+
+//start
+startButton.addEventListener("click",()=>{
+    startButton.style.display="none";
+    stopButton.style.display="block";
+    pauseButton.style.display="block";
+    timeInterval = setInterval(tick,1000);
 });
 
 //functions
-const updateTime= (time)=>timeLine.innerHTML=String(Math.trunc(time/minute)).padStart(2,"0")+":"+String(time%minute/second).padStart(2,"0");
-updateTime(gap);
+function updateTime(time){
+    timeLine.innerHTML=String(Math.trunc(time/minute)).padStart(2,"0")+":"+String(time%minute/second).padStart(2,"0");
+}
+
 function tick(){
     gap-=1000;
     console.log(String(gap%minute));
     updateTime(gap);
 }
-//if(!isPaused){
-//    setInterval(tick,1000);
-//}
 
-/*setTimeout(function run(){
-    if(!isPaused){
-        tick();
-        setTimeout(run,1000)
-    }
-},1000)*/
-timeInterval = setInterval(tick,1000);
+updateTime(gap);
