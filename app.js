036d2656c15b35311f3d4e9f8127f1659d7017ea
kushnerovM <Rojers1941@yandex.by>
2022-11-22 15:57:13 +0300

    progress bar added

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const timeLine = document.querySelector("#time-line");
 const pauseButton = document.querySelector("#pause");
 const stopButton = document.querySelector("#stop");
 const startButton = document.querySelector("#start");
-
+const progressBar = document.querySelector("#current-progress");
 //event listners
 
 //pause/resume
@@ -23,10 +23,14 @@ pauseButton.addEventListener("click",()=>{
         pauseButton.textContent="pause";
         timeInterval = setInterval(tick,1000);
         isPaused= false;
+        //progress bar configuring
+        progressBar.style.backgroundColor="#3EE08C";
     } else {
         pauseButton.textContent="resume";
         clearInterval(timeInterval);
         isPaused = true;
+        //progress bar configuring
+        progressBar.style.backgroundColor="#FFCE5C";
     }
 });
 
@@ -37,21 +41,32 @@ stopButton.addEventListener("click",()=>{
     updateTime(interval);
     gap = interval;
     stopButton.style.display="none";
-    startButton.style.display="block";
     pauseButton.style.display="none";
     //pause reset if it was set
     if(isPaused){
         pauseButton.textContent="pause";
         isPaused = false;
     }
+    //progress bar configuring
+    progressBar.style.backgroundColor="#F64740";
+    progressBar.style.transition =" width 1s linear";
+    progressBar.style.width=`0%`;
+    progressBar.addEventListener('transitionend',()=>{
+        startButton.style.display="block";
+        progressBar.style.transition =" none";
+    });
 });
 
 //start
 startButton.addEventListener("click",()=>{
     startButton.style.display="none";
     stopButton.style.display="block";
-    pauseButton.style.display="block";
+    pauseButton.style.display="block"; 
+    //progress bar configuring
+    progressBar.style.width=`0%`;
+    progressBar.style.backgroundColor="#3EE08C";
     timeInterval = setInterval(tick,1000);
+
 });
 
 //functions
@@ -61,7 +76,9 @@ function updateTime(time){
 
 function tick(){
     gap-=1000;
-    console.log(String(gap%minute));
+    document.querySelector("#progress-bar").style.backgroundColor=(gap/1000)%2?"#B8B8B8":"#8F8F8F";
+    progressBar.style.width=`${100-((gap*100)/interval)}%`;
+    console.log(100-((gap*100)/interval));
     updateTime(gap);
 }
 
